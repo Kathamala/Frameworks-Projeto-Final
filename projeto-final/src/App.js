81917d577bbd517d0423e5955abcb067b5f8044b
Thiago Vinicius <thiagovao2000@hotmail.com>
2021-08-23 19:46:33 -0300

    Cria requisição de API

--- projeto-final/src/App.js
+++ projeto-final/src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import api from './api';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    cartas: [],
+  }
+
+  async componentDidMount(){
+    const response = await api.get('');
+
+    console.log(response.data);
+
+    this.setState({ cartas: response.data});
+  }
+
+  render(){
+
+    const { cartas } = this.state;
+
+    return(
+      <div>
+          <h1>Listar</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+/*
+
+          { console.log(cartas) }
+          {cartas.map(elemento => (
+            <li key={elemento.show.id}>
+              <h2>
+                <strong>Título: </strong>
+                {elemento.show.name}
+              </h2>
+            </li>
+          ))}
+*/
